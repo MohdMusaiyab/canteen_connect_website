b190ef2a3bc0540f9c2b97499b85913e7e729bd6
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-03-19 11:43:31 +0530

    Pages Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-
-
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 function App() {
-  
-
   return (
-    <div className='text-center text-red-500'>
-      heksnjmas s
-    </div>
-  )
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-yellow-400 py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img src="logo.png" alt="Logo" className="h-10 mr-4" />
+          <h1 className="text-white font-semibold text-xl">FoodDeliveryApp</h1>
+        </div>
+
+        <nav className="hidden md:flex space-x-4">
+          <Link to="/" className="text-white hover:text-yellow-100">
+            Home
+          </Link>
+
+          <Link to="/about" className="text-white hover:text-yellow-100">
+            About
+          </Link>
+          <Link to="/contact" className="text-white hover:text-yellow-100">
+            Contact
+          </Link>
+          <Link className="text-white hover:text-yellow-100">Login</Link>
+        </nav>
+
+        {/* Mobile Menu Button (Hidden on larger screens) */}
+        <button className="md:hidden text-white focus:outline-none">
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
