6d2e807c3301aebbcc1756678b42ef36fecc48ce
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-09-06 15:42:42 +0530

    Redux for Orders

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,16 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
+import orderReducer from "./orders/orderSlice";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["user"], // only persist the user slice
+  whitelist: ["user","orders"], // only persist the user slice
 };
 const rootReducer = combineReducers({
   user: userReducer,
+  orders: orderReducer,
   // Add other reducers here
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
